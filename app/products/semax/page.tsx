6c5ce6c4c3033f3f458df1246be51ba8a539a089
apexxbiologics <apexxbiologicsllc@gmail.com>
2026-06-01 18:43:33 -0400

    Update page.tsx

--- app/products/semax/page.tsx
+++ app/products/semax/page.tsx
@@ -67,7 +67,7 @@ export default function SemaxPage() {
             <img
               src="/images/semax.PNG"
               alt="Semax"
-              className="w-full h-[500px] object-contain drop-shadow-[0_0_40px_rgba(37,99,235,0.3)]"
+              className="w-full h-[500px] object-contain"
             />
 
           </div>

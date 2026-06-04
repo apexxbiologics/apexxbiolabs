8ccc6933811e0e13da498bff6bef24d919e6ca80
apexxbiologics <apexxbiologicsllc@gmail.com>
2026-06-04 17:56:47 -0400

    Update page.tsx

--- app/products/kpv/page.tsx
+++ app/products/kpv/page.tsx
@@ -52,7 +52,7 @@ export default function KPVPage() {
         </a>
 
         <p className="uppercase tracking-[0.3em] text-gray-400 text-xs">
-          Apexx Biologics
+          Apexx Biolabs
         </p>
 
       </header>

96639f168a78a055bc65cd32d2a3f5acb58057f6
apexxbiologics <apexxbiologicsllc@gmail.com>
2026-06-04 17:56:45 -0400

    Update page.tsx

--- app/products/ghkcu/page.tsx
+++ app/products/ghkcu/page.tsx
@@ -52,7 +52,7 @@ export default function GHKCUPage() {
         </a>
 
         <p className="uppercase tracking-[0.3em] text-gray-400 text-xs">
-          Apexx Biologics
+          Apexx Biolabs
         </p>
 
       </header>

b1cc844cf9798150ee58423662a2e35f317dae39
apexxbiologics <apexxbiologicsllc@gmail.com>
2026-06-18 18:19:39 -0400

    Update page.tsx

--- app/products/tb500/page.tsx
+++ app/products/tb500/page.tsx
@@ -130,6 +130,12 @@ export default function TB500Page() {
 
 </div>
 
+<a
+  href="/#shop"
+  className="text-center border border-blue-700 hover:bg-blue-700 px-10 py-5 uppercase tracking-widest text-sm font-semibold transition-all rounded-lg"
+>
+  Continue Shopping
+</a>
               <a
                 href="/coas"
                 className="text-center border border-blue-700 hover:bg-blue-700 px-10 py-5 uppercase tracking-widest text-sm font-semibold transition-all rounded-lg"

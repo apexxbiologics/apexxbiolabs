ed24b8137189eaf49adf3e7da4d5ac4b1d3382d7
apexxbiologics <apexxbiologicsllc@gmail.com>
2026-06-18 18:18:17 -0400

    Update page.tsx

--- app/products/bacwater/page.tsx
+++ app/products/bacwater/page.tsx
@@ -131,6 +131,13 @@ const addToCart = () => {
 
 </div>
 
+<a
+  href="/#shop"
+  className="text-center border border-blue-700 hover:bg-blue-700 px-10 py-5 uppercase tracking-widest text-sm font-semibold transition-all rounded-lg"
+>
+  Continue Shopping
+</a>
+
               <a
                 href="/coas"
                 className="text-center border border-blue-700 hover:bg-blue-700 px-10 py-5 uppercase tracking-widest text-sm font-semibold transition-all rounded-lg"

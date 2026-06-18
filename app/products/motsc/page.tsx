1ea0fd2b13bd544d67026c4f041a3bdf354211db
apexxbiologics <apexxbiologicsllc@gmail.com>
2026-06-18 18:18:24 -0400

    Update page.tsx

--- app/products/motsc/page.tsx
+++ app/products/motsc/page.tsx
@@ -128,6 +128,13 @@ export default function MOTSCPage() {
               )}
 
               <a
+  href="/#shop"
+  className="text-center border border-blue-700 hover:bg-blue-700 px-10 py-5 uppercase tracking-widest text-sm font-semibold transition-all rounded-lg"
+>
+  Continue Shopping
+</a>
+
+              <a
                 href="/coas"
                 className="text-center border border-blue-700 hover:bg-blue-700 px-10 py-5 uppercase tracking-widest text-sm font-semibold transition-all rounded-lg"
               >

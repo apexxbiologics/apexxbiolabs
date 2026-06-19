2249816c7d6c5c57e14b1b3ccec73e83dd29558a
apexxbiologics <apexxbiologicsllc@gmail.com>
2026-06-19 19:41:40 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -527,6 +527,8 @@ alt=""
     </div>
   </div>
 
+</section>
+
   {/* QUALITY VERIFICATION */}
 <section className="py-32 px-6 bg-gradient-to-b from-black via-[#030712] to-black border-b border-blue-950">
   <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-14 items-center">

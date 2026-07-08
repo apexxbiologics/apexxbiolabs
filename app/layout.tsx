7944500db12c79e1935df024949723cef83eca04
apexxbiologics <brd218@gmail.com>
2026-07-08 19:58:55 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,19 +42,10 @@ export default function RootLayout({
     >
       <body className="min-h-screen bg-[#081526] text-white flex flex-col">
         <Navbar />
-        <body className="min-h-screen bg-[#081526] text-white flex flex-col">
-  <Navbar />
 
-  <main className="flex-1">
-    {children}
-  </main>
+        <main className="flex-1">{children}</main>
 
-  <Footer />
-</body>
-
-        <main className="flex-1">
-          {children}
-        </main>
+        <Footer />
       </body>
     </html>
   );

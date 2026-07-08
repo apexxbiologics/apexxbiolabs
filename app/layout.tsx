395a15af14eda3c9fa8c1a37952565e391866bf8
apexxbiologics <brd218@gmail.com>
2026-07-08 19:30:04 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,7 +39,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-screen bg-[#081526] text-white flex flex-col">
+        <Navbar />
+
+        <main className="flex-1">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }

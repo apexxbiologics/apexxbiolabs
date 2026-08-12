0caf9723546481e47b714f85ba9bbe08b7411201
apexxbiologics <brd218@gmail.com>
2026-08-12 12:03:44 -0400

    Update page.tsx

--- app/products/adamax/page.tsx
+++ app/products/adamax/page.tsx
@@ -215,8 +215,7 @@ export default function AdamaxPage() {
                   </svg>
 
                   <p className="text-blue-100 text-sm font-semibold uppercase tracking-wider">
-                    FREE BACTERIOSTATIC WATER WITH PURCHASE OF ANY 4 VIALS
-                  </p>
+Receive a Complimentary Gift With Any 8 Vials                  </p>
                 </div>
               </div>
 

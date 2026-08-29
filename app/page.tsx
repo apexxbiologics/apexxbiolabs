dfef1341a3e7b8d881b48d4c8b2db938242b0e74
apexxbiologics <brd218@gmail.com>
2026-08-28 20:47:08 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,7 @@ const startProductScroll = (direction: "left" | "right") => {
 
   autoScrollRef.current = setInterval(() => {
     productScrollRef.current?.scrollBy({
-      left: direction === "left" ? -18 : 18,
+      left: direction === "left" ? -28 : 28,
     });
   }, 16);
 };

99355228ac4400ee47070c33812bf00075d04bc5
apexxbiologics <brd218@gmail.com>
2026-09-02 00:15:54 -0400

    Update page.tsx

--- app/build-a-bundle/page.tsx
+++ app/build-a-bundle/page.tsx
@@ -178,7 +178,7 @@ const getBundleProductImage = (product: {
   // Use the exact blue image files from /public/images.
   const aliasGroups = [
     { matches: ["apx310mg", "apx310", "apx3"], image: "/images/apx310blue.png" },
-    { matches: ["apx320mg", "apx320"], image: "/images/apx320blue.png" },
+    { matches: ["apx320mg", "apx320"], image: "/images/apx320.png" },
     { matches: ["apx230mg", "apx230", "apx2"], image: "/images/apx230blue.png" },
     { matches: ["glutathione1500mg", "glutathione1500", "glutathione"], image: "/images/glutathione1500blue.png" },
     { matches: ["mitox120mg", "mitox120", "mitox"], image: "/images/mitox120blue.png" },
@@ -199,8 +199,8 @@ const getBundleProductImage = (product: {
     { matches: ["pe2228"], image: "/images/pe2228blue.png" },
     { matches: ["pinealon"], image: "/images/pinealonblue.png" },
     { matches: ["pt141"], image: "/images/pt141blue.png" },
-    { matches: ["semax"], image: "/images/semaxblue.PNG" },
-    { matches: ["selank"], image: "/images/selankblue.PNG" },
+    { matches: ["semax"], image: "/images/semaxblue.png" },
+    { matches: ["selank"], image: "/images/selankblue.png" },
     { matches: ["tb500"], image: "/images/tb500blue.png" },
     { matches: ["tesamorelin5mg", "tesamorelin5", "tesamorelin"], image: "/images/tesa5blue.png" },
     { matches: ["wolverine"], image: "/images/wolverineblue.png" },
